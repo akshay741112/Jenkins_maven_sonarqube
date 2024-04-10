722c43b06cef6d4113de3bfe5ba84df2f8592ef5
email <akshay@gmail.com>
2024-04-10 19:27:07 +0530

    --- Auto Git Commit ---

--- student_app.groovy
+++ student_app.groovy
@@ -29,8 +29,7 @@ pipeline {
 
     stage('Pull') {
             steps {
-                git changelog: false, poll: false, url: 'https://github.com/chetansomkuwar254/studentapp.ui.git'
-                echo 'Pull is done'
+                '
             }
         }
 
