54972b0018ff9bec650eabccc805792f741847a5
email <akshay@gmail.com>
2024-04-10 19:33:49 +0530

    --- Auto Git Commit ---

--- student_app.groovy
+++ student_app.groovy
@@ -5,31 +5,27 @@ pipeline {
         stage('Pull') {
             steps {
                 git changelog: false, poll: false, url: 'https://github.com/chetansomkuwar254/studentapp.ui.git'
-                echo 'Pull is done'
+                echo 'Pull is completed'
             }
         }
-    }
-
-    stage('Build') {
+        stage('Build') {
             steps {
                 sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
-                echo 'build is done'
+                echo 'Buils is completed with maven'
             }
         }
-
-    stage('Test') {
-            steps {
-                sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
-                        -Dsonar.projectKey=studentapp-ui \\
-                        -Dsonar.host.url=http://13.201.84.247:9000 \\
-                        -Dsonar.login=bdd331a584fc1dfb489eb88749922483b057d23d'''
-                echo 'Test is succeded'
-            }
-        }
-
-    stage('Deploy') {
-            steps {
-                echo 'Hello World'
-            }
-        }
+        stage('Test') {
+             steps {
+                 sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
+                      -Dsonar.projectKey=studentapp \\
+                      -Dsonar.host.url=http://18.118.47.233:9000 \\
+                      -Dsonar.login=3d374a434cf0dc04f6b3d4a43483195a8956e8af'''
+             }
+         }
+        // stage('Deploy') {
+        //     steps {
+        //         echo 'Deployed on docker'
+        //     }
+        // }
+    }
 }
