d805c8d8ba55608d8839ce4b9ced59c2aa017d74
email <akshay@gmail.com>
2024-04-10 19:16:14 +0530

    --- Auto Git Commit ---

--- student_app.groovy
+++ student_app.groovy
@@ -12,7 +12,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh "/opt/apache-maven-"
+                sh "/opt/apache-maven-3.9.6/bin/"
                 echo 'Hello World'
             }
         }
