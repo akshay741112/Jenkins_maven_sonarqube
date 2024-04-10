9e8096bfa7f0bf788d4e66899be18497d8b9f201
email <akshay@gmail.com>
2024-04-10 19:17:34 +0530

    --- Auto Git Commit ---

--- student_app.groovy
+++ student_app.groovy
@@ -20,7 +20,10 @@ pipeline {
     stages {
         stage('Test') {
             steps {
-                
+                sh '''mvn sonar:sonar \\
+                        -Dsonar.projectKey=studentapp-ui \\
+                        -Dsonar.host.url=http://13.201.84.247:9000 \\
+  -Dsonar.login=bdd331a584fc1dfb489eb88749922483b057d23d'''
                 echo 'Hello World'
             }
         }
