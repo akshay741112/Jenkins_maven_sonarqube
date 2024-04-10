0895b1f49f3e50070c179b8c05b2109a9ab64071
email <akshay@gmail.com>
2024-04-10 19:12:13 +0530

    --- Auto Git Commit ---

--- student_app.groovy
+++ student_app.groovy
@@ -2,37 +2,10 @@ pipeline {
     agent any
 
     stages {
-        stage('Pull') {
+        stage('Hello') {
             steps {
-                git changelog: false, poll: false, url: 'https://github.com/chetansomkuwar254/studentapp.ui.git'
-                echo 'Yes, Application repository pull is done !'
+                echo 'Hello World'
             }
-        }
-        stage('Build') {
-            steps {
-                sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
-                echo 'Yes, Application Build is done !'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \
-                    -Dsonar.projectKey=jenkins_project \
-                    -Dsonar.host.url=http://13.201.84.247:9000 \
-                    -Dsonar.login=bdd331a584fc1dfb489eb88749922483b057d23d'''
-}
-                echo 'Here we are testing '
-            }
-        stage('Quality Test') {
-            steps {
-                echo 'Here we are testing the quality'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploy Done'
-            }
-        
         }
     }
 }
