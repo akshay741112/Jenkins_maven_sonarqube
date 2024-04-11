658c1dc798c791d0bd5cf9df60c70698ba9128a2
email <akshay@gmail.com>
2024-04-11 19:34:57 +0530

    --- Auto Git Commit ---

--- student_app_2.groovy
+++ student_app_2.groovy
@@ -10,6 +10,7 @@ pipeline {
         }
         stage('Build') {
             steps {
+                sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
                 echo 'Buils is completed with maven'
             }
         }
