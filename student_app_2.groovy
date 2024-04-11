539b1fcbd38d3d1df84785c51dfbd432348eaddc
email <akshay@gmail.com>
2024-04-11 18:56:04 +0530

    --- Auto Git Commit ---

--- student_app_2.groovy
+++ student_app_2.groovy
@@ -24,7 +24,7 @@ pipeline {
             }
         }
      stages {
-        stage('diploy') {
+        stage('deployment') {
             steps {
                 
                 echo 'Hello World'
